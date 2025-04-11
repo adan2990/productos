5c2797654da23555249b8801cea0dfd1ced4cfb3
Adan Olea <adan.olea4580@alumnos.udg.mx>
2025-04-11 15:19:31 -0600

    Actividad para el uso de promesas y recuperar los datos de una API y ponerlo en tarjetas

---
+++ main.js
@@ -0,0 +1,62 @@
+const URLMain ="https://fakestoreapi.com/products/";
+const main= document.getElementsByTagName("main").item(0);
+
+function getData(){
+    const options ={"metod":"GET"};
+    fetch(URLMain)
+    .then((response)=>{
+        console.log(response,options);
+        //convierte la respuesta en JSON
+        response.json().then((res)=>{
+            // console.log(res.length);//20 productos
+            // console.log(res[2].title);
+            createCards(res);
+        }); 
+    })
+    .catch((err)=>{
+        main.insertAdjacentElement("beforeend",
+            `<div class="alert alert-danger" role="alert">
+            ${err.message}
+            </div>)
+            `);
+
+    });
+
+}//getData
+
+getData();
+
+function createCards(prods){
+
+    // main.insertAdjacentElement("beforebegin",
+    //     `hola`
+    // );
+    
+    prods.forEach((d) => {
+        //main.insertAdjacentHTML("beforebegin", `hola ${prods[d].title}`);
+        main.insertAdjacentHTML("beforebegin", 
+            // titulo: ${d.title}\n 
+            // description : ${d.description}\n
+            // category: ${d.category}\n
+            // image: ${d.image}\n
+            // ------------------------\n
+            `
+            <div class="card" style="width: 18rem; style=border-color:black border-width: medium">
+                <img src="${d.image}" class="card-img-top" alt="...">
+            <div class="card-body">
+            <h5 class="card-title">${d.title}</h5>
+            <p class="card-text">${d.description}</p>
+            </div style="margin-bottom:50px">
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item">Categoria: ${d.category}</li>
+                    <li class="list-group-item">Precio: ${d.price}</li>
+                </ul>
+            </div>          
+            `);
+        //console.log(d);
+
+
+        
+    });
+
+}//createcards
